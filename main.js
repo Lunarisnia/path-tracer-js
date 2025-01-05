30a3c295f01473d700b07aa4e14776eb36ccca3d
louna <r.arswendo.r@gmail.com>
2025-01-05 15:01:18 +0700

    feat: basic shader setup

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
 import * as THREE from 'three';
+import fragmentShader from './fragment.frag?raw'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const container = document.getElementById('container');
+let renderer;
+let scene;
+let camera;
+//let cube;
+let clock;
+let uniforms;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+init()
 
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.z = 1;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+	clock = new THREE.Clock()
 
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	container.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+	uniforms = {
+		u_time: { type: "f", value: 1.0 },
+		u_resolution: { type: "v2", value: new THREE.Vector2() },
+		u_mouse: { type: "v2", value: new THREE.Vector2() }
+	};
+
+	const geometry = new THREE.PlaneGeometry(renderer.domElement.width, renderer.domElement.height);
+	const material = new THREE.ShaderMaterial({
+		uniforms: uniforms,
+		fragmentShader: fragmentShader,
+	});
+	const mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+	renderer.setPixelRatio(window.devicePixelRatio);
+
+	onWindowResize();
+	window.addEventListener('resize', onWindowResize, false);
+
+	document.onmousemove = function(e) {
+		uniforms.u_mouse.value.x = e.pageX
+		uniforms.u_mouse.value.y = e.pageY
+	}
+	renderer.setAnimationLoop(render);
+}
+
+function onWindowResize() {
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	uniforms.u_resolution.value.x = renderer.domElement.width;
+	uniforms.u_resolution.value.y = renderer.domElement.height;
+}
 
 function render() {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	uniforms.u_time.value += clock.getDelta();
 
 	renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(render);
